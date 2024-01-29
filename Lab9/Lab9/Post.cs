e2233121d294579d5b7b20b3500a1ef957a951c3
iveniuss <i77599146613@yandex.ru>
2024-01-30 00:11:39 +0500

    real add lab9

---
+++ Lab9/Lab9/Post.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Lab9
+{
+    public class Post
+    {
+        private int _views, _comments, _reactions;
+
+        public static int ObjNum { get; private set; }
+
+        private int Views
+        {
+            set => _views = value < 0 ? 0 : value;
+            get => _views;
+        }
+        private int Comments
+        {
+            set
+            {
+                if (value > _views)
+                    _comments = _views;
+                else if (value < 0)
+                    _comments = 0;
+                else
+                    _comments = value;
+            }
+            get => _comments;
+        }
+        private int Reactions
+        {
+            set
+            {
+                if (value > _views)
+                    _reactions = _views;
+                else if (value < 0)
+                    _reactions = 0;
+                else
+                    _reactions = value;
+            }
+            get => _reactions;
+        }
+
+        public Post(int views, int comments, int reactions)
+        {
+            Views = views;
+            Comments = comments;
+            Reactions = reactions;
+            ObjNum++;
+        }
+
+        public Post()
+        {
+            Views = 0;
+            Comments = 0;
+            Reactions = 0;
+            ObjNum++;
+        }
+
+        public Post(Post post)
+        {
+            Views = post.Views;
+            Comments = post.Comments;
+            Reactions = post._reactions;
+            ObjNum++;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Просмотров: {Views} Комментариев: {Comments} Реакций: {Reactions}";
+        }
+
+        public static Post operator ++(Post post)
+        {
+            post.Views += 1;
+            return post;
+        }
+
+        public static Post operator !(Post post)
+        {
+            post.Reactions += 1;
+            return post;
+        }
+
+        public static bool operator ==(Post p1, Post p2)
+        {
+            return p1.Views == p2.Views && p1.Comments == p2.Comments && p1.Reactions == p2.Reactions;
+        }
+
+        public static bool operator !=(Post p1, Post p2)
+        {
+            return p1.Views != p2.Views || p1.Comments != p2.Comments || p1.Reactions != p2.Reactions;
+        }
+
+        public double EngRate()
+        {
+            double engRate = (Views + Comments + Reactions) / 10.0;
+            return engRate > 100 ? 100.0 : engRate;
+        }
+
+        public static double EngRateStatic(Post post)
+        {
+            double engRate = (post.Views + post.Comments + post.Reactions) / 10.0;
+            return engRate > 100 ? 100.0 : engRate;
+        }
+
+        public static implicit operator bool(Post post)
+        {
+            return post.Reactions + post.Reactions > 0 && post.Views > 0;
+        }
+
+        public static explicit operator double(Post post)
+        {
+            return post.EngRate();
+        }
+    }
+}
---
+++ Lab9/Lab9/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Lab9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Post post1 = new Post();
+            Post post2 = new Post(353, 151, 173);
+            Post post3 = new Post(post2);
+            Post post4 = new Post(post3);
+            post3 = !post3;
+            post3++;
+            Console.WriteLine(post2.ToString());
+            Console.WriteLine((double)post2);
+            Console.WriteLine(post3.ToString());
+            Console.WriteLine((double)post3);
+            Console.WriteLine(post2==post3);
+        }
+        
+    }
+}
