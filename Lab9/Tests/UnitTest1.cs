63da3be605021a36c3a158271f2135aac1c4333e
iveniuss <i77599146613@yandex.ru>
2024-02-04 14:41:47 +0500

    finish tests

--- Lab9/Tests/UnitTest1.cs
+++ Lab9/Tests/UnitTest1.cs
@@ -3,62 +3,322 @@ using Lab9;
 namespace Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PostTest
     {
         [TestMethod]
-        public void TestPost()
-        {
-            // Arrange
-            Post expectedPost1 = new Post(0, 0, 0);
-            Post expectedPost2 = new Post(400, 400, 199);
-            Post expectedPost3 = new Post(401, 400, 200);
-            string expectedString2 = "Просмотров: 400 Комментариев: 400 Реакций: 199";
-            double expectedEngRate2 = 99.9;
-            double expectedEngRate3 = 100.0;
-
-
-            // Act
-            Post post1 = new Post();
-            post1.Comments = -100;
-            post1.Views = -100;
-            Post post2 = new Post(400, 500, 500);
-            post2.Reactions = -100;
-            post2.Reactions = 199;
-            Post post3 = new Post(post2);
-            post3++;
-            post3 = !post3;
-
-            // Assert
-            Assert.AreEqual(post1, expectedPost1);
-            Assert.IsTrue(post2 == expectedPost2);
-            Assert.IsTrue(post2 != expectedPost3);
-            Assert.AreEqual(expectedEngRate2, (double)post2);
-            Assert.AreEqual(expectedEngRate3, (double)post3);
-            Assert.AreEqual(Post.EngRateStatic(post3), expectedEngRate3);
-            Assert.AreNotEqual(post2, post3);
-            Assert.IsTrue(post2);
-            Assert.AreEqual(post2.ToString(), expectedString2);
-            
-        }
-        [TestMethod]
-        public void TestPostCollection()
+        public void EmptyConstructorTest()
         {
             //Arrange
-            Post[] posts1 = { new Post() };
-            PostCollection expectedPostCol1 = new PostCollection(posts1);
+            int expectedViews = 0;
+            int expectedComments = 0;
+            int expectedReactions = 0;
 
             //Act
-            PostCollection postCol1 = new PostCollection();
-            Post[] posts2 = { new Post(345, 234, 123), new Post(573, 234, 130), new Post(121, 76, 45) };
-            PostCollection PostCol2 = new PostCollection(posts2);
+            Post post = new Post();
 
-            //Assert 
-            Assert.AreEqual(expectedPostCol1, postCol1);
-            Assert.AreNotEqual(expectedPostCol1, PostCol2 );
+            //Assert
+            Assert.AreEqual(expectedViews, post.Views);
+            Assert.AreEqual(expectedComments, post.Comments);
+            Assert.AreEqual(expectedReactions, post.Reactions);
+        }
+
+        [TestMethod]
+        public void ParameterContructorTest1() //Если значения реакций и комментариев больше просмотров
+        {
+            //Arrange
+            int expectedViews = 100;
+            int expectedComments = 100;
+            int expectedReactions = 100;
+
+            //Act
+            Post post = new Post(100, 120, 120);
+
+            //Assert
+            Assert.AreEqual(expectedViews, post.Views);
+            Assert.AreEqual(expectedComments, post.Comments);
+            Assert.AreEqual(expectedReactions, post.Reactions);
+        }
+
+        [TestMethod]
+        public void ParameterContructorTest2() //Если отрицательные значения
+        {
+            //Arrange
+            int expectedViews = 0;
+            int expectedComments = 0;
+            int expectedReactions = 0;
+
+            //Act
+            Post post = new Post(-100, -100, -100);
+
+            //Assert
+            Assert.AreEqual(expectedViews, post.Views);
+            Assert.AreEqual(expectedComments, post.Comments);
+            Assert.AreEqual(expectedReactions, post.Reactions);
+        }
+
+        [TestMethod]
+        public void CopyContructorPost()
+        {
+            //Arrange
+            Post p1 = new Post(789, 456, 123);
+            Post p2 = new Post(p1);
+            Post expectedP2 = new Post(789, 456, 124);
+
+            //Act
+            p2.Reactions += 1;
+
+            //Assert
+            Assert.AreNotEqual(p1, p2);
+            Assert.AreEqual(expectedP2, p2);
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            //Arrange
+            Post post = new Post(987, 654, 321);
+
+            //Act
+            string str = "Просмотров: 987 Комментариев: 654 Реакций: 321";
+
+            //Assert
+            Assert.AreEqual(str, post.ToString());
+        }
+
+        [TestMethod]
+        public void OperatorTest1() //Оператор !
+        {
+            //Arrange
+            Post post = new Post(987, 654, 321);
+            Post expectedPost = new Post(987, 654, 322);
+
+            //Act
+            post = !post;
+
+            //Assert
+            Assert.AreEqual(expectedPost, post);
+        }
+
+        [TestMethod]
+        public void OperatorTest2() //Оператор ++
+        {
+            //Arrange
+            Post post = new Post(987, 654, 321);
+            Post expectedPost = new Post(988, 654, 321);
+
+            //Act
+            post++;
+
+            //Assert
+            Assert.AreEqual(expectedPost, post);
+        }
+
+        [TestMethod]
+        public void OperatorTest3() //Оператор ==
+        {
+            //Arrange
+            Post post1 = new Post(987, 654, 321);
+            Post post2 = new Post(post1);
+            Post post3 = new Post();
+
+            //Act
+            bool post12 = post1 == post2;
+            bool post23 = post2 == post3;
+
+            //Assert
+            Assert.IsTrue(post12);
+            Assert.IsFalse(post23);
+        }
+
+        [TestMethod]
+        public void OperatorTest4() //Оператор !=
+        {
+            //Arrange
+            Post post1 = new Post(987, 654, 321);
+            Post post2 = new Post(post1);
+            Post post3 = new Post();
+
+            //Act
+            bool post12 = post1 != post2;
+            bool post23 = post2 != post3;
+
+            //Assert
+            Assert.IsFalse(post12);
+            Assert.IsTrue(post23);
+        }
 
 
 
+        [TestMethod]
+        public void EngRateTest1()
+        {
+            //Arrange
+            Post post = new Post(345, 234, 123);
+            double expectedEngRate = 70.2;
+
+            //Act
+            double engRate1 = post.EngRate();
+            double engRate2 = Post.EngRateStatic(post);
+
+            //Assert
+            Assert.AreEqual(expectedEngRate, engRate1);
+            Assert.AreEqual(expectedEngRate, engRate2);
+        }
+
+        [TestMethod]
+        public void EngRateTest2()
+        {
+            //Arrange
+            Post post = new Post(545, 434, 323);
+            double expectedEngRate = 100.0;
+
+            //Act
+            double engRate1 = post.EngRate();
+            double engRate2 = Post.EngRateStatic(post);
+
+            //Assert
+            Assert.AreEqual(expectedEngRate, engRate1);
+            Assert.AreEqual(expectedEngRate, engRate2);
+        }
+
+        [TestMethod]
+        public void ConversionOperatorTest1() //Явное преобразование в bool
+        {
+            //Arrange
+            Post post1 = new Post(345, 234, 123);
+            Post post2 = new Post();
+
+            //Asssert
+            Assert.IsTrue(post1);
+            Assert.IsFalse(post2);
+        }
+
+        [TestMethod]
+        public void ConversionOperatorTest2() //Неявное преобразоваие в double
+        {
+            Post post = new Post(345, 234, 123);
+            double expectedEngRate = 70.2;
+
+            //Assert
+            Assert.AreEqual(expectedEngRate, (double)post);
+        }
+    }
+
+    [TestClass]
+    public class PostCollectionTest
+    {
+        [TestMethod]
+        public void EmptyContructorTest()
+        {
+            //Arrange
+            int expectedLength = 1;
+            int expectedViews = 0;
+            int expectedComments = 0;
+            int expectedReactions = 0;
+
+            //Act
+            PostCollection posts = new PostCollection();
+
+            //Assert
+            Assert.AreEqual(expectedLength, posts.Length);
+            Assert.AreEqual(expectedViews, posts[0].Views);
+            Assert.AreEqual(expectedComments, posts[0].Comments);
+            Assert.AreEqual(expectedReactions, posts[0].Reactions);
+        }
+
+        [TestMethod]
+        public void ParameterContructorTest()
+        {
+            //Arrange
+            int expectedLength = 3;
+            Post[] expectedPosts = { new Post(345, 234, 123), new Post(456, 345, 234), new Post(567, 456, 345) };
+
+            //Act
+            PostCollection posts = new PostCollection(expectedPosts);
+
+            //Assert
+            Assert.AreEqual(expectedLength, posts.Length);
+            for (int i = 0; i < posts.Length; i++)
+                Assert.AreEqual(expectedPosts[i], posts[i]);
+        }
+
+        [TestMethod]
+        public void CopyContructorTest()
+        {
+            //Arrange
+            Post[] postsArr = { new Post(345, 234, 123), new Post(456, 345, 234), new Post(567, 456, 345) };
+            PostCollection posts1 = new PostCollection(postsArr);
+            PostCollection posts2 = new PostCollection(posts1);
+            PostCollection posts3 = new PostCollection(posts2);
+
+            //Act
+            posts3[0]++;
+
+            //Assert
+            Assert.AreEqual(posts1.Length, posts2.Length);
+            for (int i = 0; i < posts1.Length; i++)
+                Assert.AreEqual(posts1[i], posts2[i]);
+            Assert.AreNotEqual(posts2, posts3);
+        }
+
+        [TestMethod]
+        public void IndexerSetTest() 
+        {
+            //Arrange
+            Post[] postsArr = { new Post(345, 234, 123), new Post(456, 345, 234), new Post(567, 456, 345) };
+            PostCollection posts1 = new PostCollection(postsArr);
+            PostCollection posts2 = new PostCollection(posts1);
+
+            //Act
+            posts1[10] = new Post();
+
+            //Assert
+            Assert.AreEqual(posts1,posts2);
+        }
+
+        [TestMethod]
+        public void IndexerGetTest()
+        {
+            //Arrange
+            Post[] postsArr = { new Post(345, 234, 123), new Post(456, 345, 234), new Post(567, 456, 345) };
+            PostCollection posts = new PostCollection(postsArr);
+
+            //Assert
+            Assert.ThrowsException<IndexOutOfRangeException>(() => posts[10]);
+        }
+
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            //Arrange
+            Post[] postsArr = { new Post(345, 234, 123), new Post(456, 345, 234), new Post(567, 456, 345) };
+            PostCollection posts = new PostCollection(postsArr);
+            string expectedStr = "Просмотров: 345 Комментариев: 234 Реакций: 123\nПросмотров: 456 Комментариев: 345 Реакций: 234\nПросмотров: 567 Комментариев: 456 Реакций: 345";
+
+            //Act
+            string Str = posts.ToString();
+
+            //Assert
+            Assert.AreEqual(expectedStr, Str);
+        }
+
+        [TestMethod]
+        public void EngRate()
+        {
+            //Arrange
+            Post[] postsArr = { new Post(345, 234, 123), new Post(456, 345, 234), new Post(567, 456, 345) };
+            PostCollection posts = new PostCollection(postsArr);
+            double expectedEngRate = 90.07;
+
+            //Act
+            double engRate = posts.EngRate();
+
+            //Assert
+            Assert.AreEqual(expectedEngRate, engRate); 
 
         }
+
+
     }
 }
