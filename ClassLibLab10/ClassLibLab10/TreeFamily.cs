e73d01f41a1b09698fac80f8a7af5a28cdf4ff2a
Ivan Sychev <Iveniuss@outlook.com>
2024-06-13 23:49:26 +0500

    initial

---
+++ ClassLibLab10/ClassLibLab10/TreeFamily.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ClassLibLab10
+{
+    public class TreeFamily
+    {
+        public string TreeName { get; set; }
+        public string Family { get; set; }
+
+        public TreeFamily(string name, string type)
+        {
+            TreeName = name;
+            Family = type;
+        }
+    }
+}
